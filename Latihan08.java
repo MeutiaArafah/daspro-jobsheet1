b84059947ed283ee90bd520ae803860ae83c6604
MeutiaArafah <meutiaarafah@gmail.com>
2024-09-06 09:12:24 +0700

    Ini commit pertama

---
+++ Latihan08.java
@@ -0,0 +1,22 @@
+/**
+ * Latihan08
+ */
+public class Latihan08 {
+
+    public static void main(String[] args){
+        //System.out.print("Nama saya Meutia Arafah Hidayat");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama      : Meutia Arafah Hidayat");
+        System.out.println("NIM       : 244107060078");
+        System.out.println("Kelas     : 1A");
+        System.out.println("No Absen  : 08");
+        System.out.println("Prodi     : Sistem Informasi Bisnis");
+        System.out.println("Alamat    : Jl. Danau Sentani Tengah VI");
+        System.out.println("No HP     : 081216660763");
+        System.out.println("Hobi      : Bulu Tangkis");
+    }
+}
